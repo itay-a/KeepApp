2af3a98eef9cb2bba701eebbf0857b2a7cb7ef45
Or Herman <orherman4567@gmail.com>
2021-08-29 00:09:25 +0300

    good night Itay

--- js/apps/mail/services/mails.data.js
+++ js/apps/mail/services/mails.data.js
@@ -10,7 +10,7 @@ export const mails = [
     {
         id: 'e103',
         subject: 'COVID-19 Update June 26',
-        body: 'The Ministry of Health announced Friday that in view of the increase in the morbidity rate, there is an obligation to wear a mask in any place other than open space',
+        body: 'The Ministry of Health announced Friday that in view of the increase in the morbidity rate, there is an obligation to wear a mask in any place other than open space, The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 ',
         isRead: false,
         sentAt: 1551133930583,
         to: 'momo@momo.com'
@@ -18,7 +18,7 @@ export const mails = [
     {
         id: 'e101',
         subject: 'Urgent Meeting!',
-        body: 'Amid a flurry of diplomatic activity, the United Nations security council had an urgent meeting on the crisis last night',
+        body: 'Amid a flurry of diplomatic activity, the United Nations security council had an urgent meeting on the crisis last night,here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text',
         isRead: false,
         sentAt: 1551133930594,
         to: 'momo@momo.com'
@@ -26,7 +26,7 @@ export const mails = [
     {
         id: 'e104',
         subject: 'Get ready for an AMAZING day! See you on Sunday! :)',
-        body: 'This Sunday!!! Lets come together- on social!',
+        body: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
         isRead: false,
         sentAt: 1551133930549,
         to: 'momo@momo.com'
